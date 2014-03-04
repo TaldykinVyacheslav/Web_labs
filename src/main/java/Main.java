b124c3cc0b777011faadfa9b541da89373d7cdaa
Taldykin V.S <zegoline@gmail.com>
2014-03-04 19:10:22 +0300

    second commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author Taldykin V.S.
+ * @version 1.00 04.03.14 18:53
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
